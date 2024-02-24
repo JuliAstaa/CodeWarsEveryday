214e3070655b791585bfbe00e0dd80880a5371e3
juliasta702@gmail.com <juliasta702@gmail.com>
2024-02-24 18:49:40 +0800

    Day 33

---
+++ Day 33/Mumbling.js
@@ -0,0 +1,7 @@
+function accum(s) {
+  return s
+    .split("")
+    .map((char, index) => char.toUpperCase() + char.toLowerCase().repeat(index))
+    .join("-");
+}
+console.log(accum("MjtkuBovqrU"));
